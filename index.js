96608ca598472870bc7d5ef72e4543cf6b67bf2d
ArchitAgrawal14 <82366674+ArchitAgrawal14@users.noreply.github.com>
2024-02-17 20:19:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+function threeDotsMenu(){
+    document.getElementById("frame1").style.visibility="visible";
+    document.getElementById("frame1").classList.add("three_Dots");
+}
+function threeDotsMenuRemoval(){
+    document.getElementById("frame1").style.visibility="hidden";
+    document.getElementById("frame1").classList.remove("three_Dots");
+}
